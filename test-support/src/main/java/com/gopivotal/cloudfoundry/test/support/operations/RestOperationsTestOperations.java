d2ec229f9e85a36182734430bc5c299d090a44c3
Ramnivas Laddad <rladdad@gopivotal.com>
2013-12-02 14:04:27 -0800

    Make database tests insensitive to the query part of the uri
    There is a diffrence between spring-cloud and cloudfoundry-runtime on
    how database url is created (and passed on the the driver). Transitively,
    this affects auto-reconfig based on those libraries. This change removes
    the query parameters from the url obtained throught REST call. This makes
    the tests pass auto-reconfig tests before and after using spring-cloud.

--- test-support/src/main/java/com/gopivotal/cloudfoundry/test/support/operations/RestOperationsTestOperations.java
+++ test-support/src/main/java/com/gopivotal/cloudfoundry/test/support/operations/RestOperationsTestOperations.java
@@ -61,7 +61,15 @@ final class RestOperationsTestOperations extends AbstractTestOperations {
 
     @Override
     public URI dataSourceUrl() {
-        return URI.create(this.restOperations.getForObject("http://{host}/datasource-url", String.class, this.host));
+        String uriString = this.restOperations.getForObject("http://{host}/datasource-url", String.class, this.host);
+        // depending on how uri is created, it may (when using spring-cloud) or may not (when using cloudfoundry-runtime) 
+        // have the query string with user credentials. So chop off any query parameters so that tests can verify the core
+        // part of the uri (host, port, etc.)
+        int queryStringStart = uriString.indexOf('?');
+        if (queryStringStart != -1) {
+            uriString = uriString.substring(0, queryStringStart);
+        }
+        return URI.create(uriString);
     }
 
     @SuppressWarnings("unchecked")
