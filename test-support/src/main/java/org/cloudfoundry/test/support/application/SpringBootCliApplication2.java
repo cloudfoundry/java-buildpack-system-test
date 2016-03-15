055f0b962aa402b779d91987da62d38e4b19a232
Ben Hale <bhale@pivotal.io>
2016-03-15 16:27:50 -0700

    Dealing with a capitalization problem 1/2

--- test-support/src/main/java/org/cloudfoundry/test/support/application/SpringBootCliApplication2.java
+++ test-support/src/main/java/org/cloudfoundry/test/support/application/SpringBootCliApplication2.java
@@ -26,15 +26,15 @@ import org.springframework.web.client.AsyncRestOperations;
 import java.io.File;
 
 @Component
-public final class SpringBootCliApplication extends AbstractApplication {
+public final class SpringBootCliApplication2 extends AbstractApplication {
 
     @Autowired
-    SpringBootCliApplication(String buildpack,
-                             CloudFoundryOperations cloudFoundryOperations,
-                             @Value("${applications.springBootCli.location}") File location,
-                             NameFactory nameFactory,
-                             @Value("${applications.springBootCli.prefix}") String prefix,
-                             AsyncRestOperations restOperations) {
+    SpringBootCliApplication2(String buildpack,
+                              CloudFoundryOperations cloudFoundryOperations,
+                              @Value("${applications.springBootCli.location}") File location,
+                              NameFactory nameFactory,
+                              @Value("${applications.springBootCli.prefix}") String prefix,
+                              AsyncRestOperations restOperations) {
         super(buildpack, cloudFoundryOperations, location, nameFactory.getName(prefix), restOperations);
     }
 
