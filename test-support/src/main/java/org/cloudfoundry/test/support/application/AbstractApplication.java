b58e7699ab43e42a8c1933224abcab6d110365ba
Ben Hale <bhale@pivotal.io>
2019-05-23 19:02:19 -0500

    Call applications via https
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- test-support/src/main/java/org/cloudfoundry/test/support/application/AbstractApplication.java
+++ test-support/src/main/java/org/cloudfoundry/test/support/application/AbstractApplication.java
@@ -85,7 +85,7 @@ abstract class AbstractApplication implements Application {
     @Override
     public final Mono<String> request(String path) {
         return this.host
-            .flatMap(host -> Mono.<ResponseEntity<String>>create(emitter -> this.restOperations.getForEntity(String.format("http://%s%s", host, path), String.class)
+            .flatMap(host -> Mono.<ResponseEntity<String>>create(emitter -> this.restOperations.getForEntity(String.format("https://%s%s", host, path), String.class)
                 .addCallback(new ListenableFutureCallback<ResponseEntity<String>>() {
 
                     @Override
