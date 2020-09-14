d051418f49a8c4dcf8c2630e71aef8633c2270ee
Ben Hale <bhale@vmware.com>
2020-09-14 14:16:13 -0700

    JUnit 5
    Signed-off-by: Ben Hale <bhale@vmware.com>

--- tests/src/test/java/org/cloudfoundry/test/IntegrationTest.java
+++ tests/src/test/java/org/cloudfoundry/test/IntegrationTest.java
@@ -26,16 +26,13 @@ import org.cloudfoundry.test.support.application.SpringBootCliApplication;
 import org.cloudfoundry.test.support.application.SpringBootCliJarApplication;
 import org.cloudfoundry.test.support.application.WebApplication;
 import org.cloudfoundry.test.support.application.WebServlet2Application;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import reactor.test.StepVerifier;
 
 import java.time.Duration;
@@ -43,10 +40,8 @@ import java.time.Duration;
 import static org.junit.Assume.assumeTrue;
 
 @ActiveProfiles("test")
-@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = IntegrationTestConfiguration.class)
-@TestExecutionListeners({DependencyInjectionTestExecutionListener.class})
-public class IntegrationTest {
+final class IntegrationTest {
 
     @Autowired
     private ApplicationContext applicationContext;
@@ -54,56 +49,65 @@ public class IntegrationTest {
     @Autowired
     private Environment environment;
 
+    @DisplayName("DistZip")
     @Test
-    public final void distZip() {
+    void distZip() {
         isIgnored(this.environment, "distZip");
         test(this.applicationContext.getBean(DistZipApplication.class));
     }
 
+    @DisplayName("EJB")
     @Test
-    public final void ejb() {
+    void ejb() {
         isIgnored(this.environment, "ejb");
         test(this.applicationContext.getBean(EjbApplication.class));
     }
 
+    @DisplayName("Groovy")
     @Test
-    public final void groovy() {
+    void groovy() {
         isIgnored(this.environment, "groovy");
         test(this.applicationContext.getBean(GroovyApplication.class));
     }
 
+    @DisplayName("Java Main-Class")
     @Test
-    public final void javaMain() {
+    void javaMain() {
         isIgnored(this.environment, "javaMain");
         test(this.applicationContext.getBean(JavaMainApplication.class));
     }
 
+    @DisplayName("Ratpack")
     @Test
-    public final void ratpack() {
+    void ratpack() {
         isIgnored(this.environment, "ratpack");
         test(this.applicationContext.getBean(RatpackApplication.class));
     }
 
+    @DisplayName("Spring Boot CLI")
     @Test
-    public final void springBootCli() {
+    void springBootCli() {
         isIgnored(this.environment, "springBootCli");
         test(this.applicationContext.getBean(SpringBootCliApplication.class));
     }
 
+    @DisplayName("Spring Boot CLI JAR")
     @Test
-    public final void springBootCliJar() {
+    void springBootCliJar() {
         isIgnored(this.environment, "springBootCliJar");
         test(this.applicationContext.getBean(SpringBootCliJarApplication.class));
     }
 
+    @DisplayName("Web")
     @Test
-    public final void web() {
+    void web() {
         isIgnored(this.environment, "web");
         test(this.applicationContext.getBean(WebApplication.class));
     }
 
+    @DisplayName("Web Servlet 2")
     @Test
-    public final void webServlet2() {
+    void webServlet2() {
         isIgnored(this.environment, "webServlet2");
         test(this.applicationContext.getBean(WebServlet2Application.class));
     }
@@ -113,7 +117,7 @@ public class IntegrationTest {
         assumeTrue(String.format("Test is disabled via %s", key), environment.getProperty(key, boolean.class, true));
     }
 
-    private void test(Application application) {
+    private static void test(Application application) {
         application
             .request("/")
             .as(StepVerifier::create)
