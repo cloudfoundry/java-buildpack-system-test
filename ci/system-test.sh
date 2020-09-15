7c98859c9c3a6cc8d012656c0b0a056463f0b285
Ben Hale <bhale@vmware.com>
2020-09-15 06:58:48 -0700

    Polishing
    Signed-off-by: Ben Hale <bhale@vmware.com>

--- ci/system-test.sh
+++ ci/system-test.sh
@@ -7,7 +7,7 @@ source "$(dirname "$0")"/common.sh
 
 CREDENTIALS=$(cat "${ROOT}"/environment/credentials.json)
 
-TEST_HOST=$(jq -n -r --argjson credentials "${CREDENTIALS}" '"https://api.sys.\($credentials.name).cf-app.com"')
+TEST_HOST=$(jq -n -r --argjson credentials "${CREDENTIALS}" '"api.sys.\($credentials.name).cf-app.com"')
 export TEST_HOST
 
 TEST_PASSWORD=$(jq -n -r --argjson credentials "${CREDENTIALS}" '$credentials.password')
