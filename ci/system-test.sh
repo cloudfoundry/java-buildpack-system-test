c2665739b34a4b536b303a68bdc9d4aed2992c55
David O'Sullivan <31728678+pivotal-david-osullivan@users.noreply.github.com>
2025-07-07 12:19:25 +0100

    Update system-test.sh

--- ci/system-test.sh
+++ ci/system-test.sh
@@ -7,7 +7,7 @@ source "$(dirname "$0")"/common.sh
 
 CREDENTIALS=$(cat "${ROOT}"/environment/credentials.json)
 
-TEST_HOST=$(jq -n -r --argjson credentials "${CREDENTIALS}" '"api.sys.\($credentials.api_url)"')
+TEST_HOST=$(jq -n -r --argjson credentials "${CREDENTIALS}" 'api.sys.$credentials.api_url')
 export TEST_HOST
 
 TEST_PASSWORD=$(jq -n -r --argjson credentials "${CREDENTIALS}" '$credentials.password')
